63c4f0d4139100bb433ea18421881c6c3bad9f82
grafter-ca <caleb.designer1@gmail.com>
2025-07-23 00:28:11 +0200

    correct by using Next/Link inst. of a tag

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,19 +1,15 @@
+import Link from "next/link";
 import React from "react";
-import { HeaderProps } from "@/interfaces";
 
-const Header: React.FC<HeaderProps> = ({ title, links }) => (
+const Header: React.FC = () => (
   <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
-     <h1 className="text-2xl font-bold"><a href="/">{title}</a></h1>
+     <h1 className="text-2xl font-bold"><Link href="/">AirBNB Cloning</Link></h1>
     <nav>
-      <ul className="flex space-x-4">
-        {links.map((link, idx) => (
-          <li key={idx}>
-            <a href={link.href} className="hover:text-amber-200">
-              {link.label}
-            </a>
-          </li>
-        ))}
-      </ul>
+        <div className="flex space-x-4">
+        <Link href="/home">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+        </div>
     </nav>
   </header>
 );
